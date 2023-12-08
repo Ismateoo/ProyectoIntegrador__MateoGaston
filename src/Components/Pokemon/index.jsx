f8b064bed36c8cfeea7cfe966c9686b54bb44735
Mateo <mateo.nicoloff25@gmail.com>
2023-12-07 21:16:42 -0300

    estructura de carpetas 2

---
+++ src/Components/Pokemon/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function Pokemon(){
+return(
+    <div className={styles.container}>
+        
+    </div>
+)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './Components/App/App'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )

4e5a4ab274c0eebf37f33a660a705d57dd840286
Mateo <mateo.nicoloff25@gmail.com>
2023-12-07 22:15:34 -0300

    version 1 tarjeta

--- src/Components/Pokemon/index.jsx
+++ src/Components/Pokemon/index.jsx
@@ -1,9 +1,32 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
-export default function Pokemon(){
-return(
+export default function Pokemon() {
+  return (
     <div className={styles.container}>
-        
+      <div className={styles.pokeball__contenedor}>
+        <img src="public/img/Pokeball.png" alt="" />
+      </div>
+
+      <div className={styles.nombreNumero}>
+        <div className={styles.flechaNombre}>
+          <img src="public/img/arrow-left.svg" alt="" />
+          <span>Bulbasaur</span>
+        </div>
+        <span>#001</span>
+      </div>
+
+      <div className={styles.datos}>
+        <div className={styles.datos__imagen}>
+          {<img src="public/img/bulbasaur.png" alt="" />}
+        </div>
+        <div className={styles.tipos}>
+                <span className={styles.tipo}>Grass</span>
+                <span className={styles.tipo}>Poison</span>   
+        </div>
+        <div className={styles.about}>
+            
+        </div>
+      </div>
     </div>
-)
+  );
 }
